2fc2bfda77ad8c6e72f6e2607f63f5ecfe8026ae
Javier Herrero <jh7@sanger.ac.uk>
2006-09-12 14:09:12 +0000

    print some help when requested/needed

--- src/syntenygraph.cpp
+++ src/syntenygraph.cpp
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+void print_help(void);
+
 int main(int argc, char *argv[])
 {
   Graph my_graph;
@@ -19,6 +21,7 @@ int main(int argc, char *argv[])
   int min_length = 100000;
   int min_regions = 2;
   int min_anchors = 3;
+  bool help;
   bool ret;
   string this_arg;
 
@@ -33,12 +36,20 @@ int main(int argc, char *argv[])
     } else if ((this_arg == "--min-anchors") and (a < argc - 1)) {
       a++;
       min_anchors = atoi(argv[a]);
+    } else if ((this_arg == "--help") or (this_arg == "-h")) {
+      help = true;
     } else if (!filename) {
       filename = argv[a];
     } else {
       cerr << "Unknown option: " << this_arg << endl;
     }
   }
+
+  if (help or !filename) {
+    print_help();
+    exit(0);
+  }
+
   ret = my_graph.populate_from_file(filename);
   if (!ret) {
     cerr << "EXIT (Error while reading file)" << endl;
@@ -62,4 +73,21 @@ int main(int argc, char *argv[])
   my_graph.print_links(min_anchors, min_regions, min_length);
 
   return EXIT_SUCCESS;
+}
+
+
+void print_help(void)
+{
+  cout << "SyntenyGraph v0.1" << endl;
+  cout << endl;
+  cout << "Usage: sytnenygraph [options] anchors_file.txt" << endl;
+  cout << endl;
+  cout << "Options:" << endl;
+  cout << " --min-length: minimum length of final syntenic block (def: 100000)" << endl;
+  cout << " --min-regions: minimum number of region in the syntenic block (def: 2)" << endl;
+  cout << " --min-anchors: minimum number of anchors in the syntenic block (def: 3)" << endl;
+  cout << " --help: prints this help" << endl;
+  cout << endl;
+  cout << "See README file for more details." << endl;
+  cout << endl;
 }
